50bb5e61a513682482cd2647d562c2b38d5e7680
Patrick <3bperez2018@gmail.com>
2026-05-27 22:06:31 -0500

    feat: agregar endpoints protegidos y autenticacion

---
+++ pasteleria/src/main/java/com/pasteleria/demo/controller/PublicoController.java
@@ -0,0 +1,15 @@
+package com.pasteleria.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publico")
+public class PublicoController {
+
+    @GetMapping("/saludo")
+    public String saludoPublico() {
+        return "Este endpoint es público. No requiere token.";
+    }
+}
